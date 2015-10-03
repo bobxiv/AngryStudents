041ded99203e9e3da673a8e051369bb351196771
Emmanuel R. F <erojasfredini@Gmail.com>
2015-10-03 16:24:18 -0300

    Inicia el proyecto :)

---
+++ desktop/src/com/pdm/angrystudents/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pdm.angrystudents.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pdm.angrystudents.AngryStudentsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AngryStudentsGame(), config);
+	}
+}
---
+++ html/src/com/pdm/angrystudents/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pdm.angrystudents.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pdm.angrystudents.AngryStudentsGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new AngryStudentsGame();
+        }
+}
